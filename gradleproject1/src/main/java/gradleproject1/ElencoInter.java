89e5d5af493f541c323f187f81703c40820a8d3b
Fabricio <Fabricio@Fabricio>
2021-08-06 12:39:16 -0300

    Teste

--- gradleproject1/src/main/java/gradleproject1/ElencoInter.java
+++ gradleproject1/src/main/java/gradleproject1/ElencoInter.java
@@ -7,14 +7,16 @@ public class ElencoInter {
     float altura;
     float peso;
     String posicao;
+    int ncam;
    
-    public ElencoInter(String n, int i, float a, float p, String p1){
+    public ElencoInter(String n, int i, float a, float p, String p1, int cam){
         
         this.nome = n;
         this.idade = i;
         this.altura = a;
         this.peso = p;
         this.posicao = p1;
+        this.ncam = cam;
         
     }
     
@@ -25,6 +27,7 @@ public class ElencoInter {
         System.out.printf("Altura: %.2f CM\n", altura);
         System.out.printf("Peso: %.2f KG\n", peso);
         System.out.printf("Posicao: %s\n", posicao);
+        System.out.printf("Numero da camisa: %d\n", ncam);
         System.out.println("");
         System.out.println("");
         
--- gradleproject1/src/main/java/gradleproject1/Main.java
+++ gradleproject1/src/main/java/gradleproject1/Main.java
@@ -5,20 +5,19 @@ public class Main {
  
     public static void main(String[] args) {
         
-        ElencoInter e1 = new ElencoInter("Guerrero", 37, 1.85f, 82f, "Centro-Avante");
+        ElencoInter e1 = new ElencoInter("Guerrero", 37, 1.85f, 82f, "Centro-Avante", 9);
         
         e1.status();
         
-        ElencoInter e2 = new ElencoInter ("Cuesta", 32, 1.87f, 84f, "Zagueiro");
+        ElencoInter e2 = new ElencoInter ("Cuesta", 32, 1.87f, 84f, "Zagueiro", 15);
         
         e2.status(); 
        
-        
-        ElencoInter e3 = new ElencoInter("Edenilson", 31, 1.75f, 76f, "Meio-Campo");
+        ElencoInter e3 = new ElencoInter("Edenilson", 31, 1.75f, 76f, "Meio-Campo", 8);
         
         e3.status();
         
-        ElencoInter e4 = new ElencoInter("Patrick", 29, 1.77f, 78f, "Meio-Campo");
+        ElencoInter e4 = new ElencoInter("Patrick", 29, 1.77f, 78f, "Meio-Campo", 88);
         
         e4.status();
         
