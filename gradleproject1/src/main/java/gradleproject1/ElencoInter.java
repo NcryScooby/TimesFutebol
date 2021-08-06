eef32720ae1c936ceca4a86515f0d072c9745279
Fabricio <Fabricio@Fabricio>
2021-08-06 12:35:09 -0300

    Teste

--- gradleproject1/src/main/java/gradleproject1/ElencoInter.java
+++ gradleproject1/src/main/java/gradleproject1/ElencoInter.java
@@ -6,22 +6,25 @@ public class ElencoInter {
     int idade;
     float altura;
     float peso;
+    String posicao;
    
-    public ElencoInter(String n, int i, float a, float p){
+    public ElencoInter(String n, int i, float a, float p, String p1){
         
         this.nome = n;
         this.idade = i;
         this.altura = a;
         this.peso = p;
+        this.posicao = p1;
         
     }
     
     public void status(){
         
-        System.out.println("Nome: "+ nome);
-        System.out.println("Idade: "+ idade);
-        System.out.println("Altura: "+ altura);
-        System.out.println("Peso: "+ peso);
+        System.out.printf("Nome: %s\n", nome);
+        System.out.printf("Idade: %d\n", idade);
+        System.out.printf("Altura: %.2f CM\n", altura);
+        System.out.printf("Peso: %.2f KG\n", peso);
+        System.out.printf("Posicao: %s\n", posicao);
         System.out.println("");
         System.out.println("");
         
--- gradleproject1/src/main/java/gradleproject1/Main.java
+++ gradleproject1/src/main/java/gradleproject1/Main.java
@@ -5,20 +5,20 @@ public class Main {
  
     public static void main(String[] args) {
         
-        ElencoInter e1 = new ElencoInter("Guerrero", 37, 1.85f, 82f);
+        ElencoInter e1 = new ElencoInter("Guerrero", 37, 1.85f, 82f, "Centro-Avante");
         
         e1.status();
         
-        ElencoInter e2 = new ElencoInter ("Cuesta", 32, 1.87f, 84f);
+        ElencoInter e2 = new ElencoInter ("Cuesta", 32, 1.87f, 84f, "Zagueiro");
         
         e2.status(); 
        
         
-        ElencoInter e3 = new ElencoInter("Edenilson", 31, 1.75f, 76f);
+        ElencoInter e3 = new ElencoInter("Edenilson", 31, 1.75f, 76f, "Meio-Campo");
         
         e3.status();
         
-        ElencoInter e4 = new ElencoInter("Patrick", 29, 1.77f, 78f);
+        ElencoInter e4 = new ElencoInter("Patrick", 29, 1.77f, 78f, "Meio-Campo");
         
         e4.status();
         
